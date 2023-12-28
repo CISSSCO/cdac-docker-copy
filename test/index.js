e55ce7b56a8a3d69e66c44d27c6d982375c7b8c2
Cisco Ramon <cisssco581b@gmail.com>
2023-12-28 22:43:24 +0530

    added files

---
+++ test/index.js
@@ -0,0 +1,21 @@
+//Import the express package module here
+const express = require('express'); 
+//Create an instance to initialize express
+const app = express(); 
+
+//Define the port number
+const  port = 3000; 
+
+//Define a GET request handler
+app.get('/', (req, res) => { 
+
+  // Create and send the response to the GET endpoint
+  res.send('Hello Cisco! Please Run Me Locally'); 
+});
+
+//Run the server on the port defined above
+app.listen(port, () => { 
+
+  //Add a log when the server starts
+  console.log(`Server started on port ${port}`); 
+});
